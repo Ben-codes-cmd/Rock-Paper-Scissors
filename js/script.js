84b97379cd9c7d5f5718a93428bbf8b43da09468
Ben Jordan <ben254378@gmail.com>
2021-10-05 13:06:16 -0700

    cleaned up comments and refactored readouts

--- js/script.js
+++ js/script.js
@@ -2,103 +2,103 @@
 // 10/4/21
 // Rock Paper Scissors
 
-function round(){
+function round() {
     let victory;
     let human;
     let computer;
     let winner = false;
-    // game loop, while no winner has been declared
+
     while (!winner) {
-        // gather input
         human = getInput();
         computer = generateChoice();
-        // display matchup
-        console.log(`You have chosen ${human}... Computer has chosen ${computer}`);
+
+        console.log(`You have chosen ${human}... Computer has chosen ${computer}`); // display matchup
+
         // evaluate matchup
+
         if (human === computer) {
-            // if tie
             console.log("The match ends in a tie! Go again.");
         }
-        else{
+        else {
             winner = true;
-            // determine winner
-            victory = getWinner(human, computer);
-            // display the outcome
-            display(victory, computer, human)
+            victory = getWinner(human, computer); // determine winner
+            display(victory, computer, human) // display the outcome
             return victory;
         }
     }
 }
-function getInput(){
+function getInput() {
     let raw;
-    while (true){
+    while (true) {
         raw = prompt("Please input rock, paper or scissors: ").toLowerCase();
-        // verify valid option
-        if (raw === "rock" || raw === "paper" || raw === "scissors"){
+
+        // verify raw string
+
+        if (raw === "rock" || raw === "paper" || raw === "scissors") {
             return raw;
-        }else{
+        } else {
             console.log("Please input a valid choice.");
         }
     }
 }
 
 function generateChoice() {
-    const options = ["rock", "paper", "scissors"];
-    // random # from 0, 2
-    let choice = Math.floor(Math.random() * 3);
-    return options[choice];
+    const OPTIONS = ["rock", "paper", "scissors"];
+    let choice = Math.floor(Math.random() * 3); // random # from 0, 2
+    return OPTIONS[choice];
 }
 
 function getWinner(human, computer) {
     let res;
-    // Human chooses rock 
-    if (human === "rock"){
+    // rock 
+    if (human === "rock") {
         (computer === "scissors") ? res = true : res = false;
     }
-    // Human chooses paper
-    else if (human === "paper"){
+    // paper
+    else if (human === "paper") {
         (computer === "rock") ? res = true : res = false;
     }
-    // Human chooses scissors
-    else{
+    // scissors
+    else {
         (computer === "paper") ? res = true : res = false;
     }
     return res;
 }
 
-function display(victory, computer, human){
-    // winner
-    if (victory){
+function display(victory, computer, human) {
+    // Log outcome
+    if (victory) {
         console.log(`Victory! ${human} beats ${computer}.`);
     }
-    // loser
-    else{
+    else {
         console.log(`Defeat! ${computer} beats ${human}.`);
     }
 }
 
-// execution
+function game(num) {
+    let wins = 0
+    const rounds = num;
+    let minWins = Math.ceil(rounds / 2); // determine best # of wins out of max rounds
 
-let wins = 0
-const rounds = 7;
-let minWins = Math.ceil(rounds/2); // determine best # of wins out of max rounds
+    for (let i = 1; i < rounds + 1; i++) {
 
-for(let i = 1; i < rounds + 1; i++){
-    console.log(`Round ${i}:`) // display the current round
-    if(round()){
-        // if the human wins, increment
-        wins++;
+        console.log(`Round ${i}:`) // display current round
+
+        if (round()) { // True represents a win
+            wins++;
+        }
+
+        console.log(`${wins}-${i - wins}`) // print score ex. 1-2
+
+        if (minWins - wins > rounds - i) { // check if the game is won
+            console.log(`Better luck next time...you just got pseudo-randomly owned!`);
+            break
+        }
+        else if (wins >= minWins) {
+            console.log(`Congratulations! You have won the series with the best ${wins}/${rounds} wins`);
+            break
+        }
     }
-    // print score
-    console.log(`${wins}-${i - wins}`)
-    // check if the game is a lost cause
-    if (minWins - wins > rounds - i){
-        break
-    }
 }
-if (wins >= minWins){
-    console.log(`Congratulations! You have won the series with the best ${wins}/${rounds} wins`);
-}
-else{
-    console.log(`Better luck next time...you lost the series with ${wins} wins`)
-}
+
+game(7);
